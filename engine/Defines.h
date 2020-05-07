32ac14fdb9d0019e11702ae66cf466f87e8ae523
Mateusz Wilanowski <karuzelandia@gmail.com>
2020-05-07 22:14:36 +0200

    Fix to make project work on secondary pc

--- engine/Defines.h
+++ engine/Defines.h
@@ -4,7 +4,7 @@
 
 #define MATH_PI 3.141592653f
 
-#define DEFINE_TYPE_HASH( Type ) inline static uint32_t typeHash = GetTypeHash( #Type )
+#define DEFINE_TYPE_HASH( Type ) public: inline static uint32_t typeHash = GetTypeHash( #Type )
 #define GET_TYPE_HASH( Type ) GetTypeHash( #Type )
 
 namespace axlt {
